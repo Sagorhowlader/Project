b5c70b9689c56086f1d67dc6b1cab84e9c5be547
Sagorhowlader <sagor.iubat17@gmail.com>
2021-01-12 00:29:57 +0600

    first commit1

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+from .models import *
+
+
+@login_required
+def ProductListView(request):
+    qs = Product.objects.all()
+
+    context = {'qs': qs}
+
+    return render(request, 'product/productlist.html', context)
